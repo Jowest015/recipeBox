430f6ccf10a1915d3ac6b720558ac1219dd02cd2
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-07-17 11:31:56 -0500

    set up hbs and css

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const dbroutes = require('./src/recipe/routes');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 2340;
 
+// View engine setup
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs',
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname, +'/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json());
 
 app.get('/', (req, res)=> {
-  res.send('Hello world');
+  res.render("home");
 });
 
 app.use('/api/v1/recipes', dbroutes);
