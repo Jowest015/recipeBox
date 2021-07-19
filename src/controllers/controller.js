2ecd565271bbe53f2e13a42cead4a551c6b7ae34
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-07-18 20:06:57 -0500

    added search function -- troubleshoot

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -14,12 +14,36 @@ pool.on("error", (err, client) => {
   process.exit(-1);
 });
 
+// Show db data
 exports.view = (req, res) => {
 
   // callback - checkout a client
   pool.connect((err, client, done) => {
     if (err) throw err;
     client.query("SELECT * FROM recipedb", (err, rows) => {
+
+      if(!err) {
+        res.render("recipes", rows);
+        done();
+        
+      } else {
+        console.log(err.stack);
+      }
+      // console.log('data from table: \n', rows);
+    });
+  });
+};
+
+// Search function
+
+exports.find = (req, res) => {
+
+
+  let search = req.body.search;
+
+  pool.connect((err, client, done) => {
+    if (err) throw err;
+    client.query("SELECT * FROM recipedb WHERE name LIKE ?", ['%' + search + '%'], (err, rows) => {
 
       if(!err) {
         res.render("recipes", rows);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ const controller = require('../controllers/controller');
 // Functionality
 router.get('/', (req, res) => {res.render('home') })
 router.get('/recipes', controller.view);
+router.post('/', controller.find);
 
 
 module.exports = router;
